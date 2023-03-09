34364177137d114fb1565d4f0a2616701b2ce062
Maxl <maximilian.nowak@outlook.at>
2023-03-09 12:03:26 +0100

    without state

--- app/src/main/java/com/example/ld_02/MainActivity.kt
+++ app/src/main/java/com/example/ld_02/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.ld_02
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.FavoriteBorder
+import androidx.compose.material.icons.rounded.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.ld_02.models.Movie
+import com.example.ld_02.models.getMovies
 import com.example.ld_02.ui.theme.Ld02Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +33,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = Color.Black
                 ) {
-                    Greeting("Android")
+                    listOfMovie(movieList = getMovies())
                 }
             }
         }
@@ -34,10 +47,59 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun movieItem(
+    curr : Movie
+) {
+
+    Card(shape = RoundedCornerShape(25.dp),modifier = Modifier.padding(0.dp,10.dp)) {
+        Column() {
+            Box() {
+                Image(
+                    painter = painterResource(id = R.drawable.comments_for_beautiful_pictures),
+                    contentDescription = "image",
+                    modifier = Modifier.fillMaxWidth(),
+                    contentScale = ContentScale.FillWidth
+                )
+                Icon(
+                    imageVector = Icons.Rounded.FavoriteBorder,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(horizontal = 15.dp, vertical = 15.dp),
+                    tint = Color.White,
+                )
+
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = curr.title)
+                Icon(
+                    imageVector = Icons.Rounded.KeyboardArrowUp,
+                    contentDescription = null,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun listOfMovie(movieList: List<Movie>) {
+    LazyColumn (){
+        items(movieList){movie -> movieItem(curr = movie)}
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Ld02Theme {
-        Greeting("Android")
+
     }
 }
